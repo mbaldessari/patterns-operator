a4f1c2b49583d4ba84ff41fc29fc4628d1beae89
Michele Baldessari <michele@acksyn.org>
2024-02-14 10:49:15 +0100

    Make sure RBACs work with Role and namespace

--- controllers/helm.go
+++ controllers/helm.go
@@ -258,7 +258,6 @@ func isChartDeployed(name, namespace string) (bool, error) {
 
 	status := action.NewList(actionConfig)
 	status.AllNamespaces = false
-	// We are only interested in deployed charts
 	status.Deployed = true
 	releases, err := status.Run()
 
